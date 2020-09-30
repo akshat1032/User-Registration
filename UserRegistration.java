eebc52f6ab4a98576bcae1dd7f045c3288d40961
akshat1032 <akshat.30oct@gmail.com>
2020-09-30 17:07:00 +0530

    Added : Welcome message added

---
+++ UserRegistration.java
@@ -0,0 +1,10 @@
+package com.capgemini.userregistration;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		
+		//Welcome message
+		System.out.println("Welcome to User Registration Program");
+	}
+}
